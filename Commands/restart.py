97802196140cc61ddf49b723a291d9000c1c513b
Thomas Cleveland <thomaspcleveland@gmail.com>
2021-03-01 06:52:15 -0600

    Adjusted restart.py

--- Commands/restart.py
+++ Commands/restart.py
@@ -9,11 +9,13 @@
 
 async def command(bot, message, guildData=None):
 	if (bot.isAdmin(message.author.id)):
-		user = bot.utils.stringifyUser(message.author)
-		bot.alert(f"Restart ordered by {user}.", True, True)
+		await message.add_reaction(bot.getFrequentEmoji('accepted'))
+		user = bot.stringifyUser(message.author)
+		bot.alert(f"Restart ordered by {user}.", True)
 		await bot.restart()
 
 help = {
 	'arguments': [],
-	'summary': "Restarts the bot."
+	'summary': "Restarts the bot.",
+	'hidden': True
 }
