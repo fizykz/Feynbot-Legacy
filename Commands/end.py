8a975dd901efcb177bac2659c66faf57a77ed84c
Thomas Cleveland <thomaspcleveland@gmail.com>
2021-03-01 06:51:50 -0600

    Adjusted end.py

--- Commands/end.py
+++ Commands/end.py
@@ -8,8 +8,9 @@
 #
 async def command(bot, message, guildData=None):
 	if (bot.isAdmin(message.author.id)):
-		user = bot.utils.stringifyUser(message.author)
-		bot.alert(f"Bot closure ordered by {user}.", True, True)
+		await message.add_reaction(bot.getFrequentEmoji('accepted'))
+		user = bot.stringifyUser(message.author)
+		bot.alert(f"Bot closure ordered by {user}.", True)
 		await bot.end()
 
 help = {
