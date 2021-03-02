92ea6464a3ab7298bce9a4f81f6c5680fafac282
Thomas Cleveland <thomaspcleveland@gmail.com>
2021-03-02 05:47:39 -0600

    Added ping.py command file

---
+++ Commands/ping.py
@@ -0,0 +1,22 @@
+#def command(bot, message, taskQueue, guildData=None):
+#	return #whatever
+#
+#help = {
+#	'arguments': [],	
+#	'summary': ""
+#}
+#
+import datetime
+
+async def command(bot, message, guildData=None):
+	start = message.created_at
+	ourMessage = await message.channel.send(bot.getFrequentEmoji('loading') + " Pong!")
+	end = ourMessage.created_at
+	total = int((end - start) / datetime.timedelta(microseconds=1) / 1000 / 2) #Microsends in between, divided by 1000 to milli, then divided by two. 
+	await ourMessage.edit(content = bot.getFrequentEmoji('acceptedStatic') + " Pong: " + str(total) + " ms.")
+
+help = {
+	'name': "Ping",
+	'aliases': ["p"],
+	'summary': "Measures the average one-way latency for the bot."
+}
