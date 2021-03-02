3324797266b8dc4172cd86e6d3fe3b18249e983d
Thomas Cleveland <thomaspcleveland@gmail.com>
2021-03-02 13:08:53 -0600

    Updated resolveBooleanPrompt() and added substringIfStartsWith() with UpdateOverIterable()

--- utils.py
+++ utils.py
@@ -29,9 +29,11 @@ class TaskQueue():
 
 def resolveBooleanPrompt(string):
 	string = string.lower()
-	string = re.sub(r' .*', ' ', string)
-	string = re.sub(r'[^A-Za-z0-9 ]', '', string)
-
+	#string = re.sub(r' .*', ' ', string) #What the fuck was I writing?????
+	#string = re.sub(r'[^A-Za-z0-9 ]', '', string)
+	if (not string.isalnum()):
+		return None 
+		
 	accepted = re.match(r'y[a-z]*\s*$', string) or re.search(r'enabl[a-z]*\s*$', string) or re.search(r'true\s*$', string) or re.search(r'on\s*$', string) or re.search(r't\s*$', string)
 	denied = re.match(r'n[a-z]*\s*$', string) or re.search(r'disabl[a-z]*\s*$', string) or re.search(r'false\s*$', string) or re.search(r'off\s*$', string) or re.search(r'f\s*$', string)
 
@@ -42,6 +44,21 @@ def resolveBooleanPrompt(string):
 	elif (denied):
 		return False
 	return None
+
+def substringIfStartsWith(string, substring):
+	if string.startswith(substring):
+		return string[len(substring):]
+	return None
+
+def updateOverIterable(iterable, function):
+	if (isinstance(iterable, collections.abc.Mapping)):
+		for index in iterable.keys():
+			iterable[index] = function(iterable[index])
+	elif (isinstance(iterable), list):
+		for index in range(len(list)):
+			iterable[index] = function(iterable[index])
+	else:
+		raise TypeError("An iterable was given that wasn't a dictionary nor an array.")
 
 def fileToJson(path):
 	"""Converts a JSON file and returns the table"""
