304d7f6f85596cf850873719a3118a781cfaabc8
Thomas Cleveland <thomaspcleveland@gmail.com>
2021-03-02 05:47:50 -0600

    Added verbose.py command file

---
+++ Commands/verbose.py
@@ -0,0 +1,21 @@
+#def command(bot, message, taskQueue, guildData=None):
+#	return #whatever
+#
+#help = {
+#	'arguments': [],	
+#	'summary': ""
+#}
+#
+
+async def command(bot, message, guildData=None):
+	if (bot.isAdmin(message.author.id)):
+		bot.runConcurrently(message.add_reaction(bot.getFrequentEmoji('accepted')))
+		user = bot.stringifyUser(message.author)
+		bot.log(f"Restart ordered by {user}.", True, True)
+		await bot.restart()
+
+help = {
+	'arguments': [],
+	'summary': "Enables admin access.",
+	'hidden': True
+}
