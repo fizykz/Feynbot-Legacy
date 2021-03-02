1349632a7b8599336eb9aa9510552b63d799a434
Thomas Cleveland <thomaspcleveland@gmail.com>
2021-03-02 13:10:48 -0600

    Updated restart.py for CommandInstances

--- Commands/restart.py
+++ Commands/restart.py
@@ -1,18 +1,9 @@
-#def command(bot, message, taskQueue, guildData=None):
-#	return #whatever
-#
-#help = {
-#	'arguments': [],	
-#	'summary': ""
-#}
-#
+async def command(cmd):
+	if (cmd.isAdmin()):
+		await cmd.bot.addReaction(message, cmd.bot.getFrequentEmoji('accepted'), concurrently = True)
+		cmd.bot.alert(f"Restart ordered by {cmd.getFullUsername()}.", True)
+		await cmd.bot.restart()
 
-async def command(bot, message, guildData=None):
-	if (bot.isAdmin(message.author.id)):
-		await bot.addReaction(message, bot.getFrequentEmoji('accepted'))
-		user = bot.stringifyUser(message.author)
-		bot.alert(f"Restart ordered by {user}.", True)
-		await bot.restart()
 info = {
 	'name': "Restart",
 	'arguments': [],
