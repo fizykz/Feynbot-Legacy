bca4c835b301e7851555408801847431325675bb
Thomas Cleveland <thomaspcleveland@gmail.com>
2021-03-02 18:49:39 -0600

    Updated ping.py

--- Commands/ping.py
+++ Commands/ping.py
@@ -8,12 +8,12 @@
 #
 import datetime
 
-async def command(bot, message, guildData=None):
-	start = message.created_at
-	ourMessage = await message.channel.send(bot.getFrequentEmoji('loading') + " Pong!")
+async def command(cmd):
+	start = cmd.message.created_at
+	ourMessage = await cmd.bot.	replyMessage(cmd.message, cmd.bot.getFrequentEmoji('loading') + " Pong!")
 	end = ourMessage.created_at
 	total = int((end - start) / datetime.timedelta(microseconds=1) / 1000 / 2) #Microsends in between, divided by 1000 to milli, then divided by two. 
-	await ourMessage.edit(content = bot.getFrequentEmoji('acceptedStatic') + " Pong: " + str(total) + " ms.")
+	await ourMessage.edit(content = cmd.bot.getFrequentEmoji('acceptedStatic') + " Pong: " + str(total) + " ms.")
 
 info = {
 	'name': "Ping",
