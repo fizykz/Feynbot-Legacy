9db1f297d6d21a2b50de6770ba060b6b090fc770
Thomas Cleveland <thomaspcleveland@gmail.com>
2021-03-02 18:49:43 -0600

    Updated reload.py

--- Commands/reload.py
+++ Commands/reload.py
@@ -1,11 +1,13 @@
 async def command(cmd):
 	if (cmd.isAdmin()):
-		cmd.bot.addReaction(message, cmd.bot.getFrequentEmoji('accepted'))
+		cmd.bot.addReaction(cmd.message, cmd.bot.getFrequentEmoji('accepted'))
 		cmd.bot.alert(f"Reload ordered by {cmd.getFullUsername()}.", True)
 		cmd.bot.reloadCommands()
 		cmd.bot.reloadEvents()
+		cmd.bot.reloadLibraries()
 
 info = {
+	'name': "Reload",
 	'arguments': [],
 	'summary': "Reloads all commands.",
 	'hidden': True
