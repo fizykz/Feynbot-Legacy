91d439444fadf9baed27d9fe5f3407e8fa5a108f
Thomas Cleveland <thomaspcleveland@gmail.com>
2021-06-27 14:41:21 -0500

    Added more command documentation/better example files.

---
+++ MetaprojectFiles/blankCommandTemplate.py
@@ -0,0 +1,21 @@
+info = {									
+	'name': "Version",						
+	'arguments': [],						
+	'aliases': [],							
+	'summary': "Just a random command!",	
+	'hidden': True, 						
+	'DMs': False,							
+	'DMsOnly': False						
+}
+
+state = {}				
+
+async def startup(bot):	
+	pass 
+
+async def init(bot):	
+	pass 
+
+async def command(self):
+	pass
+
